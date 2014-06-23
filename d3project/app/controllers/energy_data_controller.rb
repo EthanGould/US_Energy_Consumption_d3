65bdb3ed876708bc06ddf3269d0d60959bffc4e9
Ethan Gould <ethan.g.907@gmail.com>
2014-06-22 21:11:10 -0400

    moved data_years method from model to controller

--- d3project/app/controllers/energy_data_controller.rb
+++ d3project/app/controllers/energy_data_controller.rb
@@ -4,8 +4,17 @@ class EnergyDataController < ApplicationController
   # GET /energy_data
   # GET /energy_data.json
   def index
-    @energy_data = EnergyDatum.response("AK")
-    @return_data = @energy_data["series"][0]["data"]
+    @api_response = EnergyDatum.response("AK")
+    @return_data = @api_response["series"][0]["data"]
+    @graph_years = data_years(@return_data)
+  end
+
+  def data_years(array)
+    @years = Array.new
+    array.each do |array|
+      @years << array[0]
+    end
+    return @years
   end
 
   private
