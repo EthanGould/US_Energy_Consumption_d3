aa5d57c5196ff52c446e75094ff081935f32f633
Ethan Gould <ethan.g.907@gmail.com>
2014-06-24 22:39:41 -0400

    query API with user specified state from form

--- d3project/app/controllers/energy_data_controller.rb
+++ d3project/app/controllers/energy_data_controller.rb
@@ -1,10 +1,13 @@
 class EnergyDataController < ApplicationController
-  before_action :set_energy_datum, only: [:show, :edit, :update, :destroy]
 
   # GET /energy_data
   # GET /energy_data.json
   def index
-    @api_response = EnergyDatum.response("AK")
+    if params[:state].nil?
+      @api_response = EnergyDatum.response("AK")
+    else
+      @api_response = EnergyDatum.response(params[:state][:state])
+    end
     @return_data = @api_response["series"][0]["data"]
     @graph_years = data_years(@return_data)
   end
@@ -12,19 +15,20 @@ class EnergyDataController < ApplicationController
   def data_years(array)
     @years = Array.new
     array.each do |array|
-      @years << array[0]
+      @years << array[0].to_i
     end
     return @years
+    # @years = Hash.new
+    # array.each do |year, number|
+    #   @years[year] = number
+    # end
+    # return @years
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_energy_datum
-      @energy_datum = EnergyDatum.find(params[:id])
-    end
-
+    def
     # Never trust parameters from the scary internet, only allow the white list through.
     def energy_datum_params
-      params[:energy_datum]
+      params.require(:energy_datum).permit(:state)
     end
 end
