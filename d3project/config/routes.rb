9dea630547147577418cdf597ecc8d01b84e1f7a
Ethan Gould <ethan.g.907@gmail.com>
2014-06-30 21:54:52 -0400

    cleaned up routes

--- d3project/config/routes.rb
+++ d3project/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "energy_data#index"
-  get 'datachart', to: 'energy_data#show'
-  post 'datachart', to: 'energy_data#show'
+  get 'datachart', to: 'energy_data#index'
+  post 'datachart', to: 'energy_data#index'
   resources :energy_data
 
 end
