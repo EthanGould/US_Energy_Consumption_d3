1b1568a39196085ae01cf78b9107ffd18d649882
Ethan Gould <ethan.g.907@gmail.com>
2014-07-03 11:32:21 -0400

    added routes for angular

--- d3project/config/routes.rb
+++ d3project/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "energy_data#index"
-  get 'datachart', to: 'energy_data#index'
-  post 'datachart', to: 'energy_data#index'
-  resources :energy_data
+
+  root to: 'energy_data#index'
+  get '/show', to: 'energy_data#show'
+  get 'angular/index'
+  get '/json', to: 'energy_data#api_json'
 
 end
