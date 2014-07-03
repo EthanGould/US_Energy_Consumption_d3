8c6b48aaac3476ecf626188e832252068c418aff
Ethan Gould <ethan.g.907@gmail.com>
2014-07-03 16:07:27 -0400

    created basic $scope data with one $scope function

--- d3project/app/assets/javascripts/projectcontroller.js
+++ d3project/app/assets/javascripts/projectcontroller.js
@@ -1,13 +1,16 @@
-app.controller('ProjectController', function ($scope, $http){
+ProjectApp.controller('ProjectController', function ($scope){
 
-  $scope.myData = [];
+  $scope.myData = [
+    {name: 'Ethan', city: 'Boston'},
+    {name: 'Hanna', city: 'Beluga'},
+    {name: 'George', city: 'New York'}
+  ];
 
-  $scope.handleData = function(data, status){
-    $scope.myData = data;
-    console.log(data);
-  };
-
-  $scope.fetch = function(){
-    $http.get('http://localhost:3000/json').success($scope.handleData);
+  $scope.addCustomer = function () {
+    $scope.myData.push(
+      {
+        name: $scope.newCustomer.name,
+        city: $scope.newCustomer.city
+      });
   };
 });
