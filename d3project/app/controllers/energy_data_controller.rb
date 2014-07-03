9be3c907273c0ac4c38b0c86d3740f35ad326352
Ethan Gould <ethan.g.907@gmail.com>
2014-07-03 11:21:12 -0400

    fixed availablity issue with user search params

--- d3project/app/controllers/energy_data_controller.rb
+++ d3project/app/controllers/energy_data_controller.rb
@@ -1,29 +1,30 @@
 class EnergyDataController < ApplicationController
-  respond_to :json
+  # respond_to :json
   # GET /energy_data
   # GET /energy_data.json
   def index
-    if params[:search]
-      @return_data = EnergyDatum.response(params[:search])
-    else
-      @return_data = EnergyDatum.response("NY")
-    end
-    years_array = return_data["series"][0]["data"]
-    d3_data = data_years(years_array)
-    binding.pry
+    @@search = params[:search]
   end
 
-  def data_years(array)
-    array.map do |item|
-      {year: item[0], amount: item[1]}
+
+  def show
+    if @@search
+      return_data = EnergyDatum.response(@@search)
+    else
+      return_data = EnergyDatum.response("NY")
     end
+    years_array = return_data["series"][0]["data"].reverse!
+    @d3_data = years_to_hash(years_array)
+    render json: @d3_data
+  end
+
+  def years_to_hash(array)
+    array.map { |item| {year: item[0], amount: item[1]} }
   end
 
   private
-    def
     # Never trust parameters from the scary internet, only allow the white list through.
     def energy_datum_params
       params.require(:energy_datum).permit(:search)
-      binding.pry
     end
 end
