6f36565050eada55f7c3a10a8d91bb334097b769
Ethan Gould <ethan.g.907@gmail.com>
2014-07-04 16:47:38 -0400

    removed angular tester $scope data

--- d3project/app/assets/javascripts/projectcontroller.js
+++ d3project/app/assets/javascripts/projectcontroller.js
@@ -1,16 +1,3 @@
 ProjectApp.controller('ProjectController', function ($scope){
 
-  $scope.myData = [
-    {name: 'Ethan', city: 'Boston'},
-    {name: 'Hanna', city: 'Beluga'},
-    {name: 'George', city: 'New York'}
-  ];
-
-  $scope.addCustomer = function () {
-    $scope.myData.push(
-      {
-        name: $scope.newCustomer.name,
-        city: $scope.newCustomer.city
-      });
-  };
 });
