4d1679c34fa67e4e2e71672a4de09f523de0bf27
Ethan Gould <ethan.g.907@gmail.com>
2014-07-06 18:12:47 -0400

    added route for energy source method

--- d3project/config/routes.rb
+++ d3project/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'energy_data#index'
   get '/show', to: 'energy_data#show'
-  get 'angular/index'
+  get '/energy_call', to: 'energy_data#energy_call'
   get '/json', to: 'energy_data#api_json'
 
 end
