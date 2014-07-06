71ca8034153449b9dd98ac44947ba07853b8c693
Ethan Gould <ethan.g.907@gmail.com>
2014-07-06 18:08:59 -0400

    added method for api call by energy source

--- d3project/app/models/energy_datum.rb
+++ d3project/app/models/energy_datum.rb
@@ -1,8 +1,14 @@
 class EnergyDatum < ActiveRecord::Base
 
-  def self.response(state)
-    api_key = ENV["API_KEY"]
-    api_url = "http://api.eia.gov/series/?api_key=#{api_key}&series_id=ELEC.CONS_TOT.COW-#{state}-98.A"
+  @api_key = ENV["API_KEY"]
+
+  # def self.response(state)
+  #   api_url = "http://api.eia.gov/series/?api_key=#{@api_key}&series_id=ELEC.CONS_TOT.COW-#{state}-98.A"
+  #   HTTParty.get(api_url)
+  # end
+
+  def self.get_energy_type(state, source)
+    api_url = "http://api.eia.gov/series/?api_key=#{@api_key}&series_id=SEDS.#{source}.#{state}.A "
     HTTParty.get(api_url)
   end
 end
