eefe646431ee74402a8a84e33e480859ea6bf5f9
Ethan Gould <ethan.g.907@gmail.com>
2014-07-06 18:07:04 -0400

    added logic to handle energy source api call response

--- d3project/app/controllers/energy_data_controller.rb
+++ d3project/app/controllers/energy_data_controller.rb
@@ -17,11 +17,18 @@ class EnergyDataController < ApplicationController
       return_data = EnergyDatum.response(@@search)
     end
     years_array = return_data["series"][0]["data"].reverse!
-    @d3_data = years_to_hash(years_array)
-    render json: @d3_data
+    data = data_to_hash(years_array)
+    render json: data
   end
 
-  def years_to_hash(array)
+  def energy_call
+    call = EnergyDatum.get_energy_type("AK", "NGPZB")
+    energy_array = call["series"][0]["data"][0..11]
+    data = data_to_hash(energy_array)
+    render json: data
+  end
+
+  def data_to_hash(array)
     array.map { |item| {year: item[0], amount: item[1]} }
   end
 
