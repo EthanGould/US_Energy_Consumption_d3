e8364273965eaed22313ebab07db3d01eba49f56
Ethan Gould <ethan.g.907@gmail.com>
2014-07-09 10:02:51 -0400

    state energy json data from js refined and ready for d3

--- d3project/app/assets/javascripts/energy_data.js
+++ d3project/app/assets/javascripts/energy_data.js
@@ -8,23 +8,8 @@ App.getURLParameter = function (param){
   return decodeURI((RegExp(param + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]);
 };
 
-App.getData = function(){
-    var api_key = "18AB54E6F91CCCB8A299192515814BDE";
-    var state = App.getURLParameter(state);
-    // var source = App.getURLParameter(source);
-    $.ajax({
-              type: "GET",
-              url: "http://api.eia.gov/series/?api_key=" + api_key + "&series_id=ELEC.CONS_TOT.COW-" + state + "-98.A",
-              dataType: 'json',
-              success: function(result){
-                var d3Data = result.series[0].data;
-                console.log(App.arrayToHash(d3Data));
-              } // success function
-            }); // end ajax
-};
-
 App.arrayToHash = function(array){
   var returnHash = {};
-  array.forEach(function(val, i){ returnHash[val[0]] = val[1]; });
+  array.forEach(function(array){ returnHash[array.year] = array.amount; });
   return returnHash;
 };
--- d3project/app/assets/javascripts/states.js
+++ d3project/app/assets/javascripts/states.js
@@ -1,2 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  $('#state').change(App.callAPI);
+});
+
+var App = App || {};
+
+App.callAPI = function(){
+    var state_abrv = $(this).children(':selected').val();
+    $.get( "states/image_url/" + state_abrv, function( data ) {
+      console.log(data.state_abbreviation);
+
+      $("#state-image").attr("src", data.image_url);
+      $(".chart-title").text("Metric tons of coal used in " + data.state_name);
+    });
+
+    $.get("states/state_data/" + state_abrv, function(data){
+      dataArray = data.energy_data;
+      d3Data = App.arrayToHash(dataArray);
+      console.log(d3Data);
+    });
+};
+App.arrayToHash = function(array){
+    var returnHash = {};
+    array.forEach(function(array){ returnHash[array.year] = array.amount; });
+    return returnHash;
+};
