a2faff2df9abb85a277d8748157e75ae0e690bac
Ethan Gould <ethan.g.907@gmail.com>
2014-07-14 21:56:20 -0400

    manipulated data differently for pie chart. WIP

--- d3project/app/assets/javascripts/piechart.js
+++ d3project/app/assets/javascripts/piechart.js
@@ -17,19 +17,14 @@ App.pieAPI = function(){
       }
     }
     // creates a data array [{ source: "HY", amount: 20431 }]
-    var d3Data = [];
-    var pieSources = [];
-    var pieHash = {type: 0, amount: 0};
-    for (source in data){
-      pieSources.push(source);
-    }
-    for (var j = 0; j < pieSources.length; j++){
-      pieHash.type = pieSources[j];
-      pieHash.amount = energyTotals[pieSources[j]];
-      d3Data.push(pieHash);
-    }
-    console.log(pieHash);
-    App.makePieChart(d3Data);
+    // var d3Data = [];
+    // var pieHash = {};
+    // energyTotals.forEach(function(source){
+    //   pieHash.type = source;
+    //   pieHash.amount = energyTotals[source];
+    // });
+    //   d3Data.push(pieHash);
+    App.makePieChart(energyTotals);
   });
 };
 
@@ -50,18 +45,13 @@ App.makePieChart = function(dataHash){
       .sort(null)
       .value(function(d) { return d.amount; });
 
-  var svg = d3.select(".col-md-6  ").append("svg")
+  var svg = d3.select(".col-md-6").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  d3.json(dataHash, function(error, data) {
-
-    data.forEach(function(d) {
-      d.amount = +d.amount;
-    });
-  });
+  data = dataHash;
 
   var g = svg.selectAll(".arc")
       .data(pie.data)
@@ -76,5 +66,5 @@ App.makePieChart = function(dataHash){
       .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
       .attr("dy", ".35em")
       .style("text-anchor", "middle")
-      .text(function(d) { return d.data.$.type(DOMelementArray);; });
+      .text(function(d) { return d.data.$.type(DOMelementArray); });
 };
